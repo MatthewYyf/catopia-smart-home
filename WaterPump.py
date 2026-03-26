8b224fcfac9ff2102d4580a9e00b79453b595565
AdamRieden <150611958+AdamRieden@users.noreply.github.com>
2026-03-25 20:15:05 -0400

    Create WaterPump.py

---
+++ WaterPump.py
@@ -0,0 +1,19 @@
+from machine import Pin
+import utime
+pump_on_time = 2
+pump_off_time = 5
+
+# turn on pump
+def turn_pump_on():
+    relay_pin = Pin(22, Pin.OUT)
+
+# turn off the pump
+def turn_pump_off():
+    relay_pin = Pin(22, Pin.IN)
+
+# Run the schedule continuously
+while True:
+    turn_pump_on()
+    utime.sleep(pump_on_time)  # Turn pump on
+    turn_pump_off()
+    utime.sleep(pump_off_time)   # Turn pump off
